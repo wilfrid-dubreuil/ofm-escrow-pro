e43add8575e43555de9b0b9041d8b0e71684adf2
Wilfrid-Dubreuil <wilfrid@familledubreuil.fr>
2026-09-21 11:34:54 +0200

    Switch Anchor localnet configuration to devnet

--- anchor-workspace/run_full_test.sh
+++ anchor-workspace/run_full_test.sh
@@ -2,7 +2,7 @@
 set -u
 export PATH=/home/wilfrid/.avm/bin:/home/wilfrid/.cargo/bin:/home/wilfrid/.local/share/solana/install/active_release/bin:$PATH
 cd /mnt/c/ofm-escrow-pro/anchor-workspace || exit 1
-export ANCHOR_PROVIDER_URL=http://127.0.0.1:8899
+export ANCHOR_PROVIDER_URL=${ANCHOR_PROVIDER_URL:-https://api.devnet.solana.com}
 export ANCHOR_WALLET=/home/wilfrid/.config/solana/id.json
 npx ts-mocha -p ./tsconfig.json -t 1000000 "tests/**/*.ts" > full-test.log 2>&1
 rc=$?
--- anchor-workspace/run_targeted_test.sh
+++ anchor-workspace/run_targeted_test.sh
@@ -2,7 +2,7 @@
 set -u
 export PATH=/home/wilfrid/.avm/bin:/home/wilfrid/.cargo/bin:/home/wilfrid/.local/share/solana/install/active_release/bin:$PATH
 cd /mnt/c/ofm-escrow-pro/anchor-workspace || exit 1
-export ANCHOR_PROVIDER_URL=http://127.0.0.1:8899
+export ANCHOR_PROVIDER_URL=${ANCHOR_PROVIDER_URL:-https://api.devnet.solana.com}
 export ANCHOR_WALLET=/home/wilfrid/.config/solana/id.json
 anchor build > targeted-build.log 2>&1
 build_rc=$?
